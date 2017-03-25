430c20db5835fa7028886197c49058c002cfe47a
Giorgio Croci <crocigiorgio0@gmail.com>
2017-03-25 11:42:49 +0100

    Inizio aggiunta fantasmi
    come tutti i miei commit, anche questo è completo a metà e verrà finito
    la prossima puntata. POLENTAH

---
+++ src/Pacman/Fantasmi.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pacman;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+
+public class Fantasmi {
+    private int x, y;
+    private Image image;
+    private boolean visible;
+    private int larghezza;
+    private int altezza;
+
+    private final static int VELOCITA_FANTASMI = -4;
+
+    public Fantasmi(int x, int y) {   
+        ImageIcon ii = new ImageIcon(this.getClass().getResource("sprites/ghost/ghost1.png"));
+        image = ii.getImage();
+        visible = true;
+        this.x = x;
+        this.y = y;
+        larghezza = image.getWidth(null);
+        altezza = image.getHeight(null);
+    }
+
+    public Rectangle getDimensionRectangle() {
+        return new Rectangle(x, y, larghezza, altezza);
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void move() {
+        x += VELOCITA_FANTASMI;
+        if (x < -70)
+            visible = false;
+    }
+    
+    
+}
+
