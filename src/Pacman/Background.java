142d94c373d0088be54b4a0c2f3c1410c802dacd
Giorgio Croci <crocigiorgio0@gmail.com>
2017-04-01 11:46:59 +0200

    Low Detail Mode e Opzioni
    wow

--- src/Pacman/Background.java
+++ src/Pacman/Background.java
@@ -7,10 +7,14 @@ package Pacman;
 import java.awt.*;
 import javax.swing.ImageIcon;
 
+
 public class Background {
     private int horiz = 200;
     private int vert = 250;
+    
     public static final Background instance = new Background();
+    private static boolean active = true;
+    
     private final Image image = new ImageIcon(this.getClass()
             .getResource("sprites/background/oro loop.png"))
             .getImage();
@@ -23,49 +27,59 @@ public class Background {
     private final Image image4 = new ImageIcon(this.getClass()
             .getResource("sprites/background/terreno.png"))
             .getImage();
+    
 
     public void paintImage(Graphics2D buffer) {   
-      //ATMOSFERA
-        int imagePos3 = -PlayCanvas.instance.getScrollPosition3();
-        while(imagePos3 < -image3.getWidth(null)) imagePos3 += image3.getWidth(null);
-        final int numImages3 = (int) Math.ceil(PlayFrame.instance.getWidth() / image3.getWidth(null)) + 2;
-        
-        for (int i = 0; i < numImages3; i++) {
-            int x = imagePos3 + i * image3.getWidth(null);
-            if (x + image3.getWidth(null) > 0) buffer.drawImage(image3, x, 0, null);
-        }
-        
-      //MONTAGNE
-        int imagePos = -PlayCanvas.instance.getScrollPosition();
-        while(imagePos < -image.getWidth(null)) imagePos += image.getWidth(null);
-        final int numImages = (int) Math.ceil(PlayFrame.instance.getWidth() / image.getWidth(null)) + 2;
-        
-        for (int i = 0; i < numImages; i++) {
-            int x = imagePos + i * image.getWidth(null);
-            if (x + image.getWidth(null) > 0) buffer.drawImage(image, x, 175, null);
-        }
-        
-        //NUVOLE
-        int imagePos2 = -PlayCanvas.instance.getScrollPosition2();
-        while(imagePos2 < -image2.getWidth(null)) imagePos2 += image2.getWidth(null);
-        final int numImages2 = (int) Math.ceil(PlayFrame.instance.getWidth() / image2.getWidth(null)) + 2;
-        
-        for (int i = 0; i < numImages2; i++) {
-            int x = imagePos2 + i * image2.getWidth(null);
-            if (x + image2.getWidth(null) > 0) buffer.drawImage(image2, x, 0, null);
-        }
-        
-      //SENTIERO
-        int imagePos4 = -PlayCanvas.instance.getScrollPosition4();
-        while(imagePos4 < -image4.getWidth(null)) imagePos4 += image4.getWidth(null);
-        final int numImages4 = (int) Math.ceil(PlayFrame.instance.getWidth() / image4.getWidth(null)) + 2;
-        
-        for (int i = 0; i < numImages4; i++) {
-            int x = imagePos4 + i * image4.getWidth(null);
-            if (x + image4.getWidth(null) > 0) buffer.drawImage(image4, x, 0, null);
-        }
-         
-        
-    }
+      
+    	if(active)
+    	{
+    		//ATMOSFERA
+			    int imagePos3 = -PlayCanvas.instance.getScrollPosition3();
+			    while(imagePos3 < -image3.getWidth(null)) imagePos3 += image3.getWidth(null);
+			    final int numImages3 = (int) Math.ceil(PlayFrame.instance.getWidth() / image3.getWidth(null)) + 2;
+			    
+			    for (int i = 0; i < numImages3; i++) {
+			        int x = imagePos3 + i * image3.getWidth(null);
+			        if (x + image3.getWidth(null) > 0) buffer.drawImage(image3, x, 0, null);
+			    }
+    	
+			    
+			  //MONTAGNE
+			int imagePos = -PlayCanvas.instance.getScrollPosition();
+			while(imagePos < -image.getWidth(null)) imagePos += image.getWidth(null);
+			final int numImages = (int) Math.ceil(PlayFrame.instance.getWidth() / image.getWidth(null)) + 2;
+			
+			for (int i = 0; i < numImages; i++) {
+			    int x = imagePos + i * image.getWidth(null);
+			    if (x + image.getWidth(null) > 0) buffer.drawImage(image, x, 175, null);
+			}
+
+			//NUVOLE
+			    int imagePos2 = -PlayCanvas.instance.getScrollPosition2();
+			    while(imagePos2 < -image2.getWidth(null)) imagePos2 += image2.getWidth(null);
+			    final int numImages2 = (int) Math.ceil(PlayFrame.instance.getWidth() / image2.getWidth(null)) + 2;
+			    
+			    for (int i = 0; i < numImages2; i++) {
+			        int x = imagePos2 + i * image2.getWidth(null);
+			        if (x + image2.getWidth(null) > 0) buffer.drawImage(image2, x, 0, null);
+			    }
+    		    
+			  //SENTIERO
+			    int imagePos4 = -PlayCanvas.instance.getScrollPosition4();
+			    while(imagePos4 < -image4.getWidth(null)) imagePos4 += image4.getWidth(null);
+			    final int numImages4 = (int) Math.ceil(PlayFrame.instance.getWidth() / image4.getWidth(null)) + 2;
+			    
+			    for (int i = 0; i < numImages4; i++) {
+			        int x = imagePos4 + i * image4.getWidth(null);
+			        if (x + image4.getWidth(null) > 0) buffer.drawImage(image4, x, 0, null);
+			    }     
+    	}	    		
+    }  
+    
+    public void setBackgroundActive()
+	{
+		active=!active;
+	}
 }
+	
 
--- src/Pacman/Menu.java
+++ src/Pacman/Menu.java
@@ -22,32 +22,48 @@ public class Menu extends Canvas implements ActionListener {
     private ImageIcon icon;
     private JLabel label;
     private JPanel p;
-
+    private boolean menu_options=false;
 
 
     public Menu() {
-
-
-
-
         //prepara la finestra
         JFrame f = new JFrame("Main Menu: Pacman");
+        JFrame o = new JFrame("Optionis: Pacman");
         JButton start=new JButton("Start Game");
+        JButton esci=new JButton("Exit");
+        JButton options=new JButton("Options");
+        JButton back=new JButton("Back");
+        JButton ldm=new JButton("Low Detail Mode");
+        
         int width=1000;
         int height=500;
         int larghezza=(int)width*2/5;
         int altezza=(int)height*75/500;
+        int larghezza2=(int)width*2/10;
+        int altezza2=(int)height*75/1000;
+        
 
         //immagine start.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/3-(PlayFrame.instance.getHeight()/3)),larghezza, altezza);
         //buttons
         start.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/2-(PlayFrame.instance.getHeight()/5)),larghezza, altezza);
         f.add(start);
-        JButton esci=new JButton("Exit");
+        options.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/2-(PlayFrame.instance.getHeight()/5))+1*altezza,larghezza, altezza);
+        f.add(options);
+        ldm.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/2-(PlayFrame.instance.getHeight()/5)),larghezza, altezza);
+        o.add(ldm);
+        back.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/2-(PlayFrame.instance.getHeight()/5))+2*altezza,larghezza, altezza);
+        o.add(back);
         esci.setBounds((PlayFrame.instance.getWidth()/2-larghezza/2),(PlayFrame.instance.getHeight()/2-(PlayFrame.instance.getHeight()/5))+2*altezza,larghezza, altezza);
         esci.setBackground(Color.BLACK);
         start.setBackground(Color.BLACK);
+        ldm.setBackground(Color.BLACK);
+        options.setBackground(Color.BLACK);
+        back.setBackground(Color.BLACK);
         start.setForeground(Color.WHITE);
         esci.setForeground(Color.WHITE);
+        ldm.setForeground(Color.WHITE);
+        options.setForeground(Color.WHITE);
+        back.setForeground(Color.WHITE);
         //operation buttons
         start.addActionListener(new ActionListener() {
             @Override
@@ -55,6 +71,31 @@ public class Menu extends Canvas implements ActionListener {
                 f.setVisible(false);
                 MainPacman.init();
             }
+        });
+        options.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	menu_options= !menu_options;
+            	f.setVisible(false);
+            	o.setVisible(true);
+            }
+   
+        });
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	menu_options= !menu_options;
+            	o.setVisible(false);
+            	f.setVisible(true);
+            }
+   
+        });
+        ldm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	Background.instance.setBackgroundActive();
+            }
+   
         });
         esci.addActionListener(new ActionListener() {
             @Override
@@ -69,6 +110,12 @@ public class Menu extends Canvas implements ActionListener {
         f.add(this);
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setVisible(true);
+        
+        o.setSize(width, height);
+        o.setResizable(false);
+        o.setLocation(100, 100);
+        o.add(this);
+        o.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     @Override
