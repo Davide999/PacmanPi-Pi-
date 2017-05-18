aef0a5e138135b69275133382d6cb6a73bf78682
Giorgio Croci <crocigiorgio0@gmail.com>
2017-05-18 09:16:41 +0200

    Tolto il warning maledetto
    Dincleberg!!!!

--- src/Pacman/PlayCanvas.java
+++ src/Pacman/PlayCanvas.java
@@ -6,7 +6,6 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
